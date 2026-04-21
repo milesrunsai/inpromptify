dc7463aa910411f735629ad9e71e3a57022cbc8e
InpromptYou <inpromptyou@gmail.com>
2026-04-21 15:55:55 +1000

    Merge remote, remove old src/ dir, install tw-animate-css, generate prisma client

--- src/app/dashboard/layout.tsx
+++
@@ -1,5 +0,0 @@
-import DashboardLayout from "@/components/DashboardLayout";
-
-export default function DashboardRootLayout({ children }: { children: React.ReactNode }) {
-  return <DashboardLayout>{children}</DashboardLayout>;
-}
--- src/lib/db.ts
+++
@@ -1,5 +0,0 @@
-import { neon } from "@neondatabase/serverless";
-
-export function getSql() {
-  return neon(process.env.DATABASE_URL!);
-}
